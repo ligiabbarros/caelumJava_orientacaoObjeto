2259ab085001200dd7eb09312d0ff60980befbaa
ligiabbarros <oo8933@caelum114-02>
2020-03-07 19:55:53 -0300

    eclipse

---
+++ Aula2/Contas.java
@@ -0,0 +1,33 @@
+
+public class Contas {
+	int numero;
+	private double saldo;
+	String titular;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
+	Contas() {
+		
+	}
+	
+	Contas(String titular){
+		
+	}
+}
---
+++ Aula2/TestarConta.java
@@ -0,0 +1,14 @@
+
+public class TestarConta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá");
+		
+		Contas contas1 = new Contas();
+		contas1.setSaldo(100000);
+		
+		System.out.println(contas1.getSaldo());
+	}
+
+}
